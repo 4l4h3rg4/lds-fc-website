2c7f196f549d8a2b73cb0111bbdfc4c5ec48a962
4l4h3rg4 <ficamatyas@gmail.com>
2025-12-15 15:18:51 -0300

    fix vercel error

--- .vercel/output/_functions/chunks/supabase_ByF6x00H.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://ggbzkuednpprfrxtmcyz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnYnprdWVkbnBwcmZyeHRtY3l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NzM3MzEsImV4cCI6MjA4MTI0OTczMX0.9YPzX3BoLCN_W7LSWIoGJyG_rwgB5bcAC8I_ccCsLEQ";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase as s };
--- .vercel/output/static/_astro/plantel.astro_astro_type_script_index_0_lang.BGSVq2Yw.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./supabase.DaSDeVGQ.js";const{data:{session:a}}=await s.auth.getSession();a||(window.location.href="/lds");
